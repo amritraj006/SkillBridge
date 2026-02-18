d569f11e5c73e610457194ac551a1a94aa37f3d1
Amrit Raj <150701418+amritraj006@users.noreply.github.com>
2026-02-18 11:33:34 +0530

    latest commit

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,20 +1,5 @@
 import axios from "axios";
 
-// const isProd = import.meta.env.MODE === "production";
-
-// const baseURL = isProd
-//   ? "https://skillbridge-hnae.onrender.com"
-//   : "http://localhost:5003";
-
-// // ✅ verification logs
-// console.log("🔍 Vite MODE:", import.meta.env.MODE);
-// console.log("🌐 API Base URL:", baseURL);
-// console.log(
-//   isProd
-//     ? "✅ Using PRODUCTION backend (Render)"
-//     : "⚙️ Using LOCAL backend (localhost)"
-// );
-
 const baseURL = import.meta.env.VITE_BACKEND_API;
 
 const api = axios.create({ baseURL });
